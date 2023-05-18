b4972f4355ddd36a778de838a1b3740fc9d40c85
cris251442 <126135249+cris251442@users.noreply.github.com>
2023-05-17 20:43:43 -0600

    Creacion de la base  de datos

---
+++ app/src/main/java/com/example/parcial4cris/DatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.parcial4cris;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "formulario.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "formulario";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NOMBRE = "nombre";
+    private static final String COLUMN_APELLIDO = "apellido";
+    private static final String COLUMN_DIRECCION = "direccion";
+    private static final String COLUMN_CIUDAD = "ciudad";
+    private static final String COLUMN_MARCA_CARRO = "marca_carro";
+    private static final String COLUMN_MODELO = "modelo";
+
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+            COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COLUMN_NOMBRE + " TEXT, " +
+            COLUMN_APELLIDO + " TEXT, " +
+            COLUMN_DIRECCION + " TEXT, " +
+            COLUMN_CIUDAD + " TEXT, " +
+            COLUMN_MARCA_CARRO + " TEXT, " +
+            COLUMN_MODELO + " TEXT)";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/parcial4cris/MainActivity.java
+++ app/src/main/java/com/example/parcial4cris/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.parcial4cris;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextNombre, editTextApellido, editTextDireccion, editTextCiudad, editTextMarcaCarro, editTextModelo;
+    private Button buttonGuardar;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+
+        editTextNombre = findViewById(R.id.editTextNombre);
+        editTextApellido = findViewById(R.id.editTextApellido);
+        editTextDireccion = findViewById(R.id.editTextDireccion);
+        editTextCiudad = findViewById(R.id.editTextCiudad);
+        editTextMarcaCarro = findViewById(R.id.editTextMarcaCarro);
+        editTextModelo = findViewById(R.id.editTextModelo);
+
+        buttonGuardar = findViewById(R.id.buttonGuardar);
+        buttonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarFormulario();
+            }
+        });
+    }
+
+    private void guardarFormulario() {
+        String nombre = editTextNombre.getText().toString().trim();
+        String apellido = editTextApellido.getText().toString().trim();
+        String direccion = editTextDireccion.getText().toString().trim();
+        String ciudad = editTextCiudad.getText().toString().trim();
+        String marcaCarro = editTextMarcaCarro.getText().toString().trim();
+        String modelo = editTextModelo.getText().toString().trim();
+
+        if (nombre.isEmpty() || apellido.isEmpty() || direccion.isEmpty() || ciudad.isEmpty()
+                || marcaCarro.isEmpty() || modelo.isEmpty()) {
+            Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("apellido", apellido);
+        values.put("direccion", direccion);
+        values.put("ciudad", ciudad);
+        values.put("marca_carro", marcaCarro);
+        values.put("modelo", modelo);
+        long resultado = db.insert("formulario", null, values);
+
+        if (resultado == -1) {
+            Toast.makeText(this, "Error al guardar el formulario", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Formulario guardado correctamente", Toast.LENGTH_SHORT).show();
+            editTextNombre.setText("");
+            editTextApellido.setText("");
+            editTextDireccion.setText("");
+            editTextCiudad.setText("");
+            editTextMarcaCarro.setText("");
+            editTextModelo.setText("");
+        }
     }
 }
